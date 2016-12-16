070eddaa688f1ff842cdbaedb4d97c9b9279f49e
James <j.nesta@gmail.com>
2016-12-16 10:02:24 -0500

    v0.0.36

--- app/main.js
+++ app/main.js
@@ -2,6 +2,9 @@
 // npm run dist --python="C:\Python27\python.exe"
 // npm run dist2 --python="C:\Python27\python.exe"
 
+// Reinstall NPM dependencies:
+// rm -rf node_modules && rm -rf app/node_modules && npm install --python="C:\Python27\python.exe"
+
 // Update with:
 // ncu --upgradeAll
 // (have to do this in both root and app directories)
