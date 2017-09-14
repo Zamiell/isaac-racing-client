e1f4488c2763dd5588e346e8a62efac673191f22
Zamiel <j.nesta@gmail.com>
2017-09-14 10:39:26 -0400

    v0.10.5

--- mod/src/RPGlobals.lua
+++ mod/src/RPGlobals.lua
@@ -4,7 +4,7 @@ local RPGlobals  = {}
 -- Global variables
 --
 
-RPGlobals.version = "v0.10.4"
+RPGlobals.version = "v0.10.5"
 
 -- These are per run
 -- (defaults are set below in the "RPGlobals:InitRun()" function)
