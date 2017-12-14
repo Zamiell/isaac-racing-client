0c3b3eb45590fb8ab1971f9f110c89cc1438a534
Zamiel <j.nesta@gmail.com>
2017-12-14 08:15:22 -0500

    v0.14.17

--- mod/src/RPGlobals.lua
+++ mod/src/RPGlobals.lua
@@ -4,7 +4,7 @@ local RPGlobals  = {}
 -- Global variables
 --
 
-RPGlobals.version = "v0.14.16"
+RPGlobals.version = "v0.14.17"
 RPGlobals.corrupted = false -- Checked in the MC_POST_GAME_STARTED callback
 RPGlobals.debug = false
 
