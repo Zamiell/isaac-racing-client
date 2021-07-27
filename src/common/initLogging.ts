3af46dbedb672f5267009ae3d067ad9af3aa4335
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-07-27 18:14:55 -0400

    change logging + better comment

--- src/common/initLogging.ts
+++ src/common/initLogging.ts
@@ -1,7 +1,9 @@
 import log from "electron-log";
 
 export default function initLogging(): void {
+  // The format is documented here:
+  // https://github.com/megahertz/electron-log/blob/master/docs/format.md
   // Even though it looks like the level and text are squished together,
   // the "electron-log" library prepends a space to the text for some reason
-  log.transports.console.format = "[{h}:{i}:{s}] [{level}]{text}";
+  log.transports.console.format = "[{h}:{i}:{s}.{ms}] [{level}]{text}";
 }
