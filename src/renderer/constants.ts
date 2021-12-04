5070faacf6c83ec8ddd126c1486c38293a5806d7
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-12-04 18:35:24 -0500

    refactoring

--- src/renderer/constants.ts
+++ src/renderer/constants.ts
@@ -1,19 +1,19 @@
 import * as remote from "@electron/remote";
 
 // true for connecting to a test server on localhost, false for connecting to the specified domain
-const localhost = false;
-const secure = true; // "true" for HTTPS/WSS and "false" for HTTP/WS
-const domain = "isaacracing.net";
-const protocolSuffix = secure && !localhost ? "s" : "";
+const LOCALHOST = false;
+const SECURE = true; // "true" for HTTPS/WSS and "false" for HTTP/WS
+const DOMAIN = "isaacracing.net";
+const PROTOCOL_SUFFIX = SECURE && !LOCALHOST ? "s" : "";
 
 export const FADE_TIME = 300; // In milliseconds
 export const IS_DEV = !remote.app.isPackaged;
 export const PBKDF2_DIGEST = "sha512"; // Digest used for password hashing
 export const PBKDF2_ITERATIONS = 1000; // Number of iterations for password hashing
 export const PBKDF2_KEYLEN = 150; // Length of resulting password hash in bits
-export const WEBSITE_URL = `http${protocolSuffix}://${
-  localhost ? "localhost" : domain
+export const WEBSITE_URL = `http${PROTOCOL_SUFFIX}://${
+  LOCALHOST ? "localhost" : DOMAIN
 }`;
-export const WEBSOCKET_URL = `ws${protocolSuffix}://${
-  localhost ? "localhost" : domain
+export const WEBSOCKET_URL = `ws${PROTOCOL_SUFFIX}://${
+  LOCALHOST ? "localhost" : DOMAIN
 }/ws`;
